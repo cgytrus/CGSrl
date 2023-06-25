a77b482303c4cf8ddeb455121a6ecc70c27b35dc
ConfiG <cgytrus@cgyt.ru>
2023-06-26 01:41:40 +0300

    fix pushing pushables into players

--- Cgsrl.Shared/src/Environment/PushableObject.cs
+++ Cgsrl.Shared/src/Environment/PushableObject.cs
@@ -6,6 +6,7 @@ namespace Cgsrl.Shared.Environment;
 public abstract class PushableObject : LevelObject {
     public bool TryMove(Vector2Int delta) {
         if(level.HasObjectAt<WallObject>(position + delta) ||
+            level.HasObjectAt<PlayerObject>(position + delta) ||
             (level.TryGetObjectAt(position + delta, out PushableObject? next) && !next.TryMove(delta)))
             return false;
         position += delta;
