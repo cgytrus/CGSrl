a6623a3fb2ebbbc9dcdd6622b87d0d5d1c4d12fa
ConfiG <cgytrus@cgyt.ru>
2023-06-28 19:16:49 +0300

    slight perlin noise on floor

--- Cgsrl.Shared/src/Environment/FloorObject.cs
+++ Cgsrl.Shared/src/Environment/FloorObject.cs
@@ -6,7 +6,20 @@ using PER.Util;
 namespace Cgsrl.Shared.Environment;
 
 public class FloorObject : SyncedLevelObject {
-    protected override RenderCharacter character { get; } = new('.', Color.transparent, new Color(0.1f, 0.1f, 0.1f, 1f));
+    private const float PerlinScale = 10f;
+    private const float PerlinValue = 0.05f;
+
+    protected override RenderCharacter character { get; } =
+        new('.', Color.transparent, new Color(0.1f, 0.1f, 0.1f, 1f));
+
     public override void Update(TimeSpan time) { }
     public override void Tick(TimeSpan time) { }
+
+    public override void Draw() {
+        float p = Perlin.Get(MathF.Abs(position.x / PerlinScale), MathF.Abs(position.y / PerlinScale), 0f) *
+            PerlinValue * 2f - PerlinValue;
+        renderer.DrawCharacter(level.LevelToScreenPosition(position),
+            new RenderCharacter(character.character, character.background,
+                character.foreground + new Color(p, p, p, 0f)));
+    }
 }
