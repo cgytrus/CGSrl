06fc90e0607933e81f7ee04ac84638a25ca2d434
ConfiG <cgytrus@cgyt.ru>
2023-07-01 00:21:44 +0300

    precalculate perlin on floor

--- Cgsrl.Shared/src/Environment/FloorObject.cs
+++ Cgsrl.Shared/src/Environment/FloorObject.cs
@@ -9,17 +9,16 @@ public class FloorObject : SyncedLevelObject {
     private const float PerlinScale = 10f;
     private const float PerlinValue = 0.05f;
 
-    protected override RenderCharacter character { get; } =
-        new('.', Color.transparent, new Color(0.1f, 0.1f, 0.1f, 1f));
+    protected override RenderCharacter character => _character;
+    private RenderCharacter _character;
+
+    public override void Added() {
+        base.Added();
+        float p = Perlin.Get(MathF.Abs(position.x / PerlinScale), MathF.Abs(position.y / PerlinScale), 0f) *
+            PerlinValue * 2f - PerlinValue;
+        _character = new RenderCharacter('.', Color.transparent, new Color(0.1f + p, 0.1f + p, 0.1f + p, 1f));
+    }
 
     public override void Update(TimeSpan time) { }
     public override void Tick(TimeSpan time) { }
-
-    public override void Draw() {
-        float p = Perlin.Get(MathF.Abs(position.x / PerlinScale), MathF.Abs(position.y / PerlinScale), 0f) *
-            PerlinValue * 2f - PerlinValue;
-        renderer.DrawCharacter(level.LevelToScreenPosition(position),
-            new RenderCharacter(character.character, character.background,
-                character.foreground + new Color(p, p, p, 0f)));
-    }
 }
