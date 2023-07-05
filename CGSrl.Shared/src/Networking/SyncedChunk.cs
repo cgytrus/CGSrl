71acde9070ca185dab62ab735fc62a32bb4bb393
ConfiG <cgytrus@cgyt.ru>
2023-07-05 22:58:01 +0300

    per

--- CGSrl.Shared/src/Networking/SyncedChunk.cs
+++ CGSrl.Shared/src/Networking/SyncedChunk.cs
@@ -2,4 +2,7 @@
 
 namespace CGSrl.Shared.Networking;
 
-public class SyncedChunk : Chunk<SyncedLevel, SyncedChunk, SyncedLevelObject> { }
+public class SyncedChunk : Chunk<SyncedLevel, SyncedChunk, SyncedLevelObject> {
+    protected override bool shouldUpdate => level.isClient;
+    protected override bool shouldTick => !level.isClient;
+}
